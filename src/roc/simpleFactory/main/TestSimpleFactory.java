1701529bed289a0ef2153c902e2fe20058bf90c3
roc97 <zkp9701@163.com>
2019-06-23 23:27:28 +0800

    修改TestSimpleFactory.java

--- src/roc/simpleFactory/main/TestSimpleFactory.java
+++ src/roc/simpleFactory/main/TestSimpleFactory.java
@@ -11,10 +11,10 @@ import roc.simpleFactory.pojo.Operation;
 public class TestSimpleFactory {
 
     public static void main(String[] args) {
-        Operation oper= OperationFactory.createOperate("*");
+        Operation oper = OperationFactory.createOperate("*");
         oper.setNumberA(2);
         oper.setNumberB(5);
-        double result=oper.GetResult();
-        System.out.println("运算结果："+result);
+        double result = oper.GetResult();
+        System.out.println("运算结果：" + result);
     }
 }
